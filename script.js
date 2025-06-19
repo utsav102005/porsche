7c273cdd93bed7c3aae2c64702a3142616880156
utsav <sojitrautsava@gmail.com>
2025-06-19 18:14:21 +0530

    script file added

---
+++ script.js
@@ -0,0 +1,13 @@
+var age = 19;
+
+if (age >= 18) {
+    console.log("above age ");
+    
+}else{
+    console.log("below age");
+    
+}
+
+document.getElementById("btn").addEventListener("click", function() {
+  alert("Button clicked!");
+})
